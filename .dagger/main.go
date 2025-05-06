fad414d8add26eae7ce5eadefe2a178979c3e544
Louis Fradin <lerenn@users.noreply.github.com>
2025-05-06 13:20:06 +0200

    fix: fix Golang version for dagger (#1)

---
+++ .dagger/main.go
@@ -0,0 +1,95 @@
+// A generated module for runtime functions
+//
+// This module has been generated via dagger init and serves as a reference to
+// basic module structure as you get started with Dagger.
+//
+// Two functions have been pre-created. You can modify, delete, or add to them,
+// as needed. They demonstrate usage of arguments and return types using simple
+// echo and grep commands. The functions can be called from the dagger CLI or
+// from one of the SDKs.
+//
+// The first line in this comment block is a short description line and the
+// rest is a long description with more detail on the module's purpose or usage,
+// if appropriate. All modules should have a short description.
+
+package main
+
+import (
+	"context"
+	"runtime"
+
+	"github.com/cryptellation/runtime/dagger/internal/dagger"
+)
+
+type Runtime struct{}
+
+// Publish a new release
+func (ci *Runtime) PublishTag(
+	ctx context.Context,
+	sourceDir *dagger.Directory,
+	user *string,
+	token *dagger.Secret,
+) error {
+	// Create Git repo access
+	repo, err := NewGit(ctx, NewGitOptions{
+		SrcDir: sourceDir,
+		User:   user,
+		Token:  token,
+	})
+	if err != nil {
+		return err
+	}
+
+	// Publish new tag
+	return repo.PublishTagFromReleaseTitle(ctx)
+}
+
+// Check returns a container that runs the checker.
+func (mod *Runtime) Check(
+	sourceDir *dagger.Directory,
+) *dagger.Container {
+	c := dag.Container().From("ghcr.io/cryptellation/checker")
+	return mod.withGoCodeAndCacheAsWorkDirectory(c, sourceDir).
+		WithExec([]string{"checker", "--check-test-tags=false"})
+}
+
+// Lint runs golangci-lint on the source code in the given directory.
+func (mod *Runtime) Lint(sourceDir *dagger.Directory) *dagger.Container {
+	c := dag.Container().
+		From("golangci/golangci-lint:v1.62.0").
+		WithMountedCache("/root/.cache/golangci-lint", dag.CacheVolume("golangci-lint"))
+
+	c = mod.withGoCodeAndCacheAsWorkDirectory(c, sourceDir)
+
+	return c.WithExec([]string{"golangci-lint", "run", "--timeout", "10m"})
+}
+
+// UnitTests returns a container that runs the unit tests.
+func (mod *Runtime) UnitTests(sourceDir *dagger.Directory) *dagger.Container {
+	c := dag.Container().From("golang:" + goVersion() + "-alpine")
+	return mod.withGoCodeAndCacheAsWorkDirectory(c, sourceDir).
+		WithExec([]string{"sh", "-c",
+			"go test ./...",
+		})
+}
+
+func goVersion() string {
+	return runtime.Version()[2:]
+}
+
+func (mod *Runtime) withGoCodeAndCacheAsWorkDirectory(
+	c *dagger.Container,
+	sourceDir *dagger.Directory,
+) *dagger.Container {
+	containerPath := "/go/src/github.com/cryptellation/runtime"
+	return c.
+		// Add Go caches
+		WithMountedCache("/root/.cache/go-build", dag.CacheVolume("gobuild")).
+		WithMountedCache("/go/pkg/mod", dag.CacheVolume("gocache")).
+
+		// Add source code
+		WithMountedDirectory(containerPath, sourceDir).
+
+		// Add workdir
+		WithWorkdir(containerPath)
+}
